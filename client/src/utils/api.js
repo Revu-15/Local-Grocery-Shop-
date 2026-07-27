cf518d3f2093c144caf169f9535169a39271f6ab
Revu-15 <polamreddyrevanth.82@gmail.com>
2026-07-27 19:14:07 +0530

    Chore: support VITE_API_BASE for production deployment

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5000/api';
 
 export const fetchProducts = async (filters = {}) => {
   const params = new URLSearchParams();
